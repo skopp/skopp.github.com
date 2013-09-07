95f1e6407c5e8987fafefaf57b5788e507ce37e0
skopp <skopp888@gmail.com>
2013-09-07 03:23:54 +0200

    .6

--- js/404.js
+++ js/404.js
@@ -1,24 +1,38 @@
-var Application = function() {
-  var a, b, e, d, f = function() {
-    for(var c = 0;c < d.length;c += 4) {
-      var a = 255 * Math.random() + 50;
-      d[c] = a;
-      d[c + 1] = a;
-      d[c + 2] = a
-    }
-    b.putImageData(e, 0, 0)
-  };
-  return{init:function() {
-    a = document.getElementById("canvas");
-    b = a.getContext("2d");
-    a.width = 700;
-    a.height = 500;
-    b.fillStyle = "white";
-    b.fillRect(0, 0, 700, 500);
-    b.fill();
-    e = b.getImageData(0, 0, 700, 500);
-    d = e.data;
-    setInterval(f, 30)
-  }}
-}();
-Application.init();
+var Application = ( function () {
+        var canvas;
+        var ctx;
+        var imgData;
+        var pix;
+        var WIDTH;
+        var HEIGHT;
+        var flickerInterval;
+
+        var init = function () {
+            canvas = document.getElementById('canvas');
+            ctx = canvas.getContext('2d');
+            canvas.width = WIDTH = 700;
+            canvas.height = HEIGHT = 500;
+            ctx.fillStyle = 'white';
+            ctx.fillRect(0, 0, WIDTH, HEIGHT);
+            ctx.fill();
+            imgData = ctx.getImageData(0, 0, WIDTH, HEIGHT);
+            pix = imgData.data;
+            flickerInterval = setInterval(flickering, 30);
+        };
+
+        var flickering = function () {
+            for (var i = 0; i < pix.length; i += 4) {
+                var color = (Math.random() * 255) + 50;
+                pix[i] = color;
+                pix[i + 1] = color;
+                pix[i + 2] = color;
+            }
+            ctx.putImageData(imgData, 0, 0);
+        };
+
+        return {
+            init: init
+        };
+    }());
+
+    Application.init();
